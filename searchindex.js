cb2247aa08cea0b8ecce13f691da4e0e3ab572df
Kaustav Roy <60354001+kaustavroy-lab@users.noreply.github.com>
2021-03-04 21:29:08 +0530

    new master

--- searchindex.js
+++ searchindex.js
@@ -80,5 +80,6 @@ function SearchDatabase()
    this[database_length++] = new SearchPage("https://dristikononline.in/script.html", "Dristikon Online Short Film", " ", "Website regarding dristikon online work");
    this[database_length++] = new SearchPage("https://dristikononline.in/webmag/webmag.html", "Dristikon WebMag", "webmag, magazines, free, submission, upload, new web, puja, ", "দৃষ্টিকোণ ম্যাগাজিন উৎসব সংখ্যা ২০২০");
    this[database_length++] = new SearchPage("https://dristikononline.in/webmag/write.html", "'দৃষ্টিকোণ ম্যাগাজিন '-এ লেখা পাঠানোর ফর্ম..", "webmag, magazines, free, submission, upload, new web, puja, ", "'দৃষ্টিকোণ ম্যাগাজিন '-এ লেখা পাঠানোর ফর্ম..");
+   this[database_length++] = new SearchPage("https://dristikononline.in/webmag/Master_Webmag.html", "'দৃষ্টিকোণ ম্যাগাজিন '-এ লেখা পাঠানোর ফর্ম..", "webmag, magazines, free, submission, upload, new web, puja, ", "'দৃষ্টিকোণ ম্যাগাজিন '-এ লেখা পাঠানোর ফর্ম..");
    return this;
 }
